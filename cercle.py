f6805adddffea8d89b74dff3ca405535bb9bb5f4
SanFran128 <sanfran128@gmail.com>
2025-12-07 20:32:26 +0100

    PI més exacte

--- cercle.py
+++ cercle.py
@@ -3,6 +3,6 @@
 # Arxiu cercle.py : càlcul perímetre d'una circumferència
 
 radi = input("Radi de la circumferència? ")
-PI = 3.14
+PI = 3.14159
 perimetre = 2*PI*float(radi)
 print("El perímetre de la circumferència de radi {} és {}".format(radi,perimetre))
